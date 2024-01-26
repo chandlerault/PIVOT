57f348b7131c72e89986d0fc7c238d07e2523d7e
DreamWeaver2k <chandlerault@gmail.com>
2024-01-26 09:41:41 -0800

    added data utils

--- PIVOT/utils/data_utils.py
+++ PIVOT/utils/data_utils.py
@@ -31,6 +31,7 @@ from io import BytesIO
 import yaml
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pymssql
 from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError
 
@@ -168,58 +169,202 @@ def get_metrics():
 
         # return table.to_pandas()
 
-
-def get_images(filtered_df, batch_size=1):
+def get_images(filepath):
     """
-    Retrieve images from Azure Blob Storage based on a filtered DataFrame. Takes ~0.25s per image.
+    Retrieve images from Azure Blob Storage based on a filepath. Takes ~0.25s per image.
 
     Args:
-        filtered_df (pandas.DataFrame): DataFrame containing filtered blob names.
-        batch_size (int, optional): Batch size for processing blobs. Defaults to 1.
+        filepath (pandas.DataFrame): DataFrame containing filtered blob names.
 
-    Yields:
-        List<PIL.Image.Image>: A list of PIL.Image.Image object of size batch_size 
-            representing images retrieved from a blob.
+    Returns:
+        PIL.Image.Image: A  PIL.Image.Image object  with image retrieved from a filepath.
     """
 
-    if not isinstance(filtered_df, pd.DataFrame):
-        raise TypeError("filtered_df must be panda dataframe")
-    if not isinstance(batch_size, int):
-        raise TypeError("batch_size must be an integer")
+    if not isinstance(filepath, str):
+        raise TypeError("filepath must be a string")
 
-    blobs = filtered_df['blob_name'].tolist()
-    for i in range(0,len(blobs),batch_size):
-        images = []
-        batch = blobs[i:i + batch_size]
 
-        # Process one batch at a time
-        for blob_name in batch:
-            # Get a BlobClient for the current blob
-            connection_string = config['connection_string2'] # TODO: eventually make this one connection string.
-            container_name =  config['image_container']
-            print(connection_string)
-            # Set up Azure clients
-            blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-            blob_exists = blob_client.exists()
+    connection_string = config['connection_string2'] # TODO: eventually make this one connection string.
+    container_name =  config['image_container']
 
-            if blob_exists:
-                pass
-            else:
-                print(f"Blob '{blob_name}' does not exist in the container.")
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=filepath)
+    blob_exists = blob_client.exists()
 
-            blob_data = blob_client.download_blob()
-            image_content = blob_data.readall()
+    if blob_exists:
+        pass
+    else:
+        print(f"Blob '{filepath}' does not exist in the container.")
 
-            if image_content.startswith(b'\x89PNG\r\n\x1a\n'):
-                print("The blob contains a valid PNG image.")
-            else:
-                print("The blob does not appear to be a valid PNG image.")
+    blob_data = blob_client.download_blob()
+    image_content = blob_data.readall()
 
-            image = Image.open(BytesIO(image_content))
-            # yield image # for single image
-            images.append([image, filtered_df.iloc[i,:]])
-            print(images)
-            # images.append([np.random.uniform(size=(124,124,1)), filtered_df.iloc[i,:]])
+    image = Image.open(BytesIO(image_content))
+
+    return image        
+
+
+# def get_images(filtered_df, batch_size=1):
+#     """
+#     Retrieve images from Azure Blob Storage based on a filtered DataFrame. Takes ~0.25s per image.
+
+#     Args:
+#         filtered_df (pandas.DataFrame): DataFrame containing filtered blob names.
+#         batch_size (int, optional): Batch size for processing blobs. Defaults to 1.
+
+#     Yields:
+#         List<PIL.Image.Image>: A list of PIL.Image.Image object of size batch_size 
+#             representing images retrieved from a blob.
+#     """
+
+#     if not isinstance(filtered_df, pd.DataFrame):
+#         raise TypeError("filtered_df must be panda dataframe")
+#     if not isinstance(batch_size, int):
+#         raise TypeError("batch_size must be an integer")
+
+#     blobs = filtered_df['blob_name'].tolist()
+#     for i in range(0,len(blobs),batch_size):
+#         images = []
+#         batch = blobs[i:i + batch_size]
+
+#         # Process one batch at a time
+#         for blob_name in batch:
+#             # Get a BlobClient for the current blob
+#             connection_string = config['connection_string2'] # TODO: eventually make this one connection string.
+#             container_name =  config['image_container']
+#             print(connection_string)
+#             # Set up Azure clients
+#             blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+#             blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+#             blob_exists = blob_client.exists()
+
+#             if blob_exists:
+#                 pass
+#             else:
+#                 print(f"Blob '{blob_name}' does not exist in the container.")
+
+#             blob_data = blob_client.download_blob()
+#             image_content = blob_data.readall()
+
+#             if image_content.startswith(b'\x89PNG\r\n\x1a\n'):
+#                 print("The blob contains a valid PNG image.")
+#             else:
+#                 print("The blob does not appear to be a valid PNG image.")
+
+#             image = Image.open(BytesIO(image_content))
+#             # yield image # for single image
+#             images.append([image, filtered_df.iloc[i,:]])
+#             print(images)
+#             # images.append([np.random.uniform(size=(124,124,1)), filtered_df.iloc[i,:]])
         
-        yield images
+#         yield images
+
+def create_user(user_info):
+    id = insert_data('users', user_info)
+    return id
+    
+def get_user(email):
+    user = select('users', {'email':email}, ['u_id']) 
+    if len(user) > 0:
+        return user[0]['u_id']
+    else:
+        return None
+
+    
+    pass # None, uid
+
+def get_models():
+    return select_distinct('models', ['model_name','m_id'])
+
+def get_dissimilarities():
+    return select_distinct('dissimilarity', ['name','d_id'])
+
+def insert_data(table_name, data):
+    """
+    Inserts data into the table with the corresponding table_name.
+
+    Args:
+        table_name (str): The name of the table in the database that the data should be inserted into.
+        data (dict/list<dict>): A single dict or list of dicts. Each key represents a column in the table and each value is a value to be inserted.
+    
+    Returns:
+        id (int, list<int>): id's of inserted data.
+    """
+    try:
+        # Define your database connection parameters
+        server = config['server']
+        database = config['database']
+        user = config['db_user']
+        password = config['db_password']
+        
+
+        with pymssql.connect(server, user, password, database) as conn:
+            with conn.cursor() as cursor:
+                if isinstance(data, list) and len(data) > 0:
+                    # Generate the INSERT statement dynamically based on the dictionary keys
+                    columns = ', '.join(data[0].keys())  # Assuming all dictionaries have the same keys
+                    placeholders = ', '.join('%(' + key + ')s' for key in data[0].keys())
+                    insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+                    # Execute the INSERT statement using executemany and the list of dictionaries
+                    cursor.executemany(insert_query, data)
+                    conn.commit()
+
+                    # TODO: dump id into temporary tables in batch call and return id's
+
+                elif isinstance(data, dict):
+                    # Generate the INSERT statement dynamically based on the dictionary keys
+                    columns = ', '.join(data.keys())  # Assuming all dictionaries have the same keys
+                    placeholders = ', '.join('%(' + key + ')s' for key in data.keys())
+                    insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+                    # Execute the INSERT statement using executemany and the list of dictionaries
+                    cursor.execute(insert_query, data)
+                    conn.commit()
+                    # Optionally, retrieve the identity of the inserted row
+                    cursor.execute("SELECT SCOPE_IDENTITY()")
+                    id = cursor.fetchone()[0]
+
+                    return id
+    except Exception as e:
+        print("Error:", str(e))
+
+def select(table_name, conditions, columns=['*']):
+    try:
+        server = config['server']
+        database = config['database']
+        user = config['db_user']
+        password = config['db_password']
+        with pymssql.connect(server=server, database=database, user=user, password=password) as conn:
+            with conn.cursor() as cursor:
+                query = f"SELECT {', '.join(columns)} FROM {table_name}"
+
+                if conditions:
+                    condition_strings = []
+                    for column, value in conditions.items():
+                        condition_strings.append(f"{column} = '{value}'")
+                    where_clause = " WHERE " + " AND ".join(condition_strings)
+                    query += where_clause
+
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                column_names = [desc[0] for desc in cursor.description]
+                result = [dict(zip(column_names, row)) for row in rows]
+                return result
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+def select_distinct(table_name, columns):
+    try:
+        server = config['server']
+        database = config['database']
+        user = config['db_user']
+        password = config['db_password']
+        with pymssql.connect(server=server, database=database, user=user, password=password) as conn:
+            with conn.cursor() as cursor:
+                query = f"SELECT DISTINCT {', '.join(columns)} FROM {table_name}"
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                column_names = [desc[0] for desc in cursor.description]
+                result = [dict(zip(column_names, row)) for row in rows]
+                return result
+    except Exception as e:
+        print(f"Error: {str(e)}")
