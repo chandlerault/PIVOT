01e40ad7e00cf88819215025426c7dcae467cd6a
DreamWeaver2k <chandlerault@gmail.com>
2024-01-26 22:33:32 -0800

    restructured utils

---
+++ PIVOT/utils/app_utils.py
@@ -0,0 +1,75 @@
+from utils import data_utils
+from PIL import Image
+import numpy as np
+import cv2
+
+def create_user(user_info):
+    """
+    Create a user from a dictionary of user_info and returns new user id.
+
+    Args: 
+        user_info (dict): Dict with keys associated to columns in user table.
+
+    Returns:
+        u_id (int): The u_id for the new user
+    """
+
+    u_id = data_utils.insert_data('users', user_info)
+    return u_id
+    
+def get_user(email):
+    """
+    Gets the u_id associated with the email if it exists. Otherwise returns None.
+
+    Args: 
+        email (str): String of a user's email.
+
+    Returns:
+        u_id (int/None): The u_id for the user with the email. Otherwise None.
+    """
+    user = data_utils.select('users', {'email':email}, ['u_id']) 
+    if len(user) > 0:
+        return user[0]['u_id']
+    else:
+        return None
+    
+def get_models():
+    """
+    Gets all the unique models and their m_id's.
+
+    Returns:
+        model_list (list<Dict>): A list of dictionaries with keys model_name and m_id.
+    """
+    return data_utils.select_distinct('models', ['model_name','m_id'])
+
+def get_dissimilarities():
+    """
+    Gets all the unique dissimilarity metrics and their d_id's.
+
+    Returns:
+        model_list (list<Dict>): A list of dictionaries with keys name and d_id.
+    """
+    return data_utils.select_distinct('dissimilarity', ['name','d_id'])
+
+def get_image(file_path):
+    """
+    Gets the image associated with the file_path. Otherwise returns None.
+
+    Args: 
+        email (str): String of a user's email.
+
+    Returns:
+        u_id (int/None): The u_id for the user with the email. Otherwise None.
+    """
+     
+    image_contents = data_utils.get_blob_bytes(file_path)
+    if image_contents.startswith(b'\x89PNG\r\n\x1a\n'):
+                print("The blob contains a valid PNG image.")
+    else:
+        print("The blob does not appear to be a valid PNG image.")
+    im = np.frombuffer(image_contents, dtype=np.uint8)
+    im = cv2.imdecode(im, cv2.IMREAD_UNCHANGED)
+    im = data_utils.preprocess_input(im)
+    # image = Image.open(BytesIO(image_contents))
+    return Image.fromarray(im.reshape(im.shape[:2]))
+
--- PIVOT/utils/data_utils.py
+++ PIVOT/utils/data_utils.py
@@ -27,149 +27,19 @@ Example:
         for image in image_set:
             # show(image)
 """
-from io import BytesIO
 import yaml
-import pyarrow as pa
-import pyarrow.parquet as pq
 import pymssql
 from azure.storage.blob import BlobServiceClient
-from azure.core.exceptions import ResourceNotFoundError
-
 from PIL import Image
 import pandas as pd
 import numpy as np
+import cv2
 
 
 CONFIG_FILE_PATH = 'config/config.yaml'
 config = yaml.load(open(CONFIG_FILE_PATH, 'r', encoding='utf-8'), Loader=yaml.FullLoader)
 
-def save_df(df, blob_name, container_name=None, connection_string=None):
-    """
-    Saves a dataframe to blob storage.
-
-    Args:
-        df (pandas.DataFrame): DataFrame containing blob_names, labels, and metrics.
-        blob_name (str): Name to save the blob to. Must end in pd, parquet, or csv.
-        container_name (str, optional): Name of container for the blob. Defaults to metrics_container in config.
-        connection_string (str, optional): Connection string for azure. Defaults to the one saved in config file.
-    """
-    try:
-        if container_name is None:
-            container_name =  config['metrics_container']
-        if connection_string is None:
-            connection_string = config['connection_string']
-
-        # Check extension
-        valid_extensions = ['parquet', 'pq', 'csv']
-        if blob_name.lower().split('.')[-1] not in valid_extensions:
-            print('s',blob_name.lower().split('.')[-1])
-            raise ValueError(f'blob_name: {blob_name} does not have a valid extension. Must be in: {valid_extensions}')
-         # Azure clients initialization
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        container_client = blob_service_client.get_container_client(container_name)
-        
-        # Create the container if it doesn't exist
-        if not container_client.exists():
-            container_client.create_container()
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-        # table = pa.Table.from_pandas(df)
-        if blob_name.lower().split('.')[-1] == 'csv':
-            # Save the DataFrame as CSV format
-            with BytesIO() as buf:
-                df.to_csv(buf, index=False)
-                buf.seek(0)
-                blob_client.upload_blob(buf.read(), overwrite=True)
-        else:
-            table = pa.Table.from_pandas(df)
-            with BytesIO() as buf:
-                pq.write_table(table, buf)
-                buf.seek(0)
-                blob_client.upload_blob(buf.read(), overwrite=True)
-
-    except ValueError as e:
-        print(str(e))
-        return None
-
-def get_df(blob_name, container_name, connection_string=None):
-    """
-    Retrieve metrics data from Azure Blob Storage and convert it to a Pandas DataFrame.
-    Args:
-        df (pandas.DataFrame): DataFrame containing blob_names, labels, and metrics.
-        blob_name (str): Name to save the blob to. Must end in pd, parquet, or csv.
-        container_name (str): Name of container for the blob.
-        connection_string (str, optional): Connection string for azure. Defaults to the one saved in config file.
-    Returns:
-        pandas.DataFrame:   A DataFrame containing the data in the blob.
-    """
-    try:
-        # Make sure the extension is correct
-        valid_extensions = ['parquet', 'pq', 'csv']
-        if blob_name.lower().split('.')[-1] not in valid_extensions:
-            raise ValueError(f'metric_blob: {blob_name} does not have a valid extension.')
-        
-        if connection_string is None:
-            connection_string = config['connection_string']
-        file_extension = blob_name.split(".")[-1].lower()  # Get the file extension
-
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-        data = blob_client.download_blob()
-        file_data = data.readall()
-
-        if file_extension == "parquet":
-            table = pq.read_table(BytesIO(file_data))
-            return table.to_pandas()
-            
-        elif file_extension == "csv":
-            return pd.read_csv(BytesIO(file_data))
-            
-        else:
-            print(f"Unsupported file format: {file_extension}")
-    except ResourceNotFoundError as _:
-        print(f"Blob '{blob_name}' not found.")
-        return None
-    except ValueError as e:
-        print(str(e))
-        return None
-
-# TODO: add catches for non-existent containers
-def get_metrics():
-    """
-    Retrieve metrics data from Azure Blob Storage and convert it to a Pandas DataFrame.
-
-    Returns:
-        pandas.DataFrame:   A DataFrame containing the metrics data.
-                            Columns: blob_name, class, metrics (can vary)
-    """
-    return get_df(blob_name=config['metrics_blob'], container_name=config['metrics_container'])
-        # # Retrieve config information for accessing Azure
-        # connection_string = config['connection_string']
-        # container_name =  config['metrics_container']
-        # # TODO: make this handle all data types (e.g. csv, tsv, etc)
-        # blob_name = config['metrics_blob'] # could make this a list of metric files and combine them.
-
-        # # Make sure the extension is correct
-        # valid_extensions = ['parquet', 'pq']
-        # if blob_name.lower().split('.')[-1] not in valid_extensions:
-        #     print(blob_name.lower().split('.')[-1])
-        #     raise ValueError(f'metric_blob: {blob_name} does not have a valid extension.')
-        
-        # # Set up Azure clients
-        # blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        # container_client = blob_service_client.get_container_client(container_name)
-
-        # # Load blob data
-        # blob_data = container_client.get_blob_client(blob_name).download_blob()
-
-        # parquet_bytes = blob_data.readall()
-
-        # table = pq.read_table(BytesIO(parquet_bytes))
-
-        # return table.to_pandas()
-
-def get_images(filepath):
+def get_blob_bytes(blob_path):
     """
     Retrieve images from Azure Blob Storage based on a filepath. Takes ~0.25s per image.
 
@@ -180,7 +50,7 @@ def get_images(filepath):
         PIL.Image.Image: A  PIL.Image.Image object  with image retrieved from a filepath.
     """
 
-    if not isinstance(filepath, str):
+    if not isinstance(blob_path, str):
         raise TypeError("filepath must be a string")
 
 
@@ -188,96 +58,18 @@ def get_images(filepath):
     container_name =  config['image_container']
 
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=filepath)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
     blob_exists = blob_client.exists()
 
     if blob_exists:
         pass
     else:
-        print(f"Blob '{filepath}' does not exist in the container.")
+        print(f"Blob '{blob_path}' does not exist in the container.")
 
     blob_data = blob_client.download_blob()
-    image_content = blob_data.readall()
+    content = blob_data.readall()
 
-    image = Image.open(BytesIO(image_content))
-
-    return image        
-
-
-# def get_images(filtered_df, batch_size=1):
-#     """
-#     Retrieve images from Azure Blob Storage based on a filtered DataFrame. Takes ~0.25s per image.
-
-#     Args:
-#         filtered_df (pandas.DataFrame): DataFrame containing filtered blob names.
-#         batch_size (int, optional): Batch size for processing blobs. Defaults to 1.
-
-#     Yields:
-#         List<PIL.Image.Image>: A list of PIL.Image.Image object of size batch_size 
-#             representing images retrieved from a blob.
-#     """
-
-#     if not isinstance(filtered_df, pd.DataFrame):
-#         raise TypeError("filtered_df must be panda dataframe")
-#     if not isinstance(batch_size, int):
-#         raise TypeError("batch_size must be an integer")
-
-#     blobs = filtered_df['blob_name'].tolist()
-#     for i in range(0,len(blobs),batch_size):
-#         images = []
-#         batch = blobs[i:i + batch_size]
-
-#         # Process one batch at a time
-#         for blob_name in batch:
-#             # Get a BlobClient for the current blob
-#             connection_string = config['connection_string2'] # TODO: eventually make this one connection string.
-#             container_name =  config['image_container']
-#             print(connection_string)
-#             # Set up Azure clients
-#             blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-#             blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-#             blob_exists = blob_client.exists()
-
-#             if blob_exists:
-#                 pass
-#             else:
-#                 print(f"Blob '{blob_name}' does not exist in the container.")
-
-#             blob_data = blob_client.download_blob()
-#             image_content = blob_data.readall()
-
-#             if image_content.startswith(b'\x89PNG\r\n\x1a\n'):
-#                 print("The blob contains a valid PNG image.")
-#             else:
-#                 print("The blob does not appear to be a valid PNG image.")
-
-#             image = Image.open(BytesIO(image_content))
-#             # yield image # for single image
-#             images.append([image, filtered_df.iloc[i,:]])
-#             print(images)
-#             # images.append([np.random.uniform(size=(124,124,1)), filtered_df.iloc[i,:]])
-        
-#         yield images
-
-def create_user(user_info):
-    id = insert_data('users', user_info)
-    return id
-    
-def get_user(email):
-    user = select('users', {'email':email}, ['u_id']) 
-    if len(user) > 0:
-        return user[0]['u_id']
-    else:
-        return None
-
-    
-    pass # None, uid
-
-def get_models():
-    return select_distinct('models', ['model_name','m_id'])
-
-def get_dissimilarities():
-    return select_distinct('dissimilarity', ['name','d_id'])
+    return content        
 
 def insert_data(table_name, data):
     """
@@ -321,9 +113,8 @@ def insert_data(table_name, data):
                     conn.commit()
                     # Optionally, retrieve the identity of the inserted row
                     cursor.execute("SELECT SCOPE_IDENTITY()")
-                    id = cursor.fetchone()[0]
-
-                    return id
+                    ids = cursor.fetchone()[0]
+                    return ids
     except Exception as e:
         print("Error:", str(e))
 
@@ -368,3 +159,25 @@ def select_distinct(table_name, columns):
                 return result
     except Exception as e:
         print(f"Error: {str(e)}")
+
+def preprocess_input(image, fixed_size=128):
+    '''
+    
+    '''
+    
+    image_size = image.shape[:2] 
+    ratio = float(fixed_size)/max(image_size)
+    new_size = tuple([int(x*ratio) for x in image_size])
+    img = cv2.resize(image, (new_size[1], new_size[0]))
+    delta_w = fixed_size - new_size[1]
+    delta_h = fixed_size - new_size[0]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    color = [0, 0, 0]
+    ri = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    #print(ri.shape)
+    # gray_image = cv2.cvtColor(ri, cv2.COLOR_BGR2GRAY)
+    gimg = np.array(ri).reshape(fixed_size,fixed_size,1)
+    #gimg = np.array(ri).reshape(fixed_size,fixed_size,1)
+    img_n = cv2.normalize(gimg, gimg, 0, 255, cv2.NORM_MINMAX)
+    return img_n
