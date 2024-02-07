84fee03cb781314f75b0fb088611d7018f875563
DreamWeaver2k <chandlerault@gmail.com>
2024-02-06 18:11:22 -0800

    merged and adjusted pylint

---
+++ PIVOT/test.py
@@ -0,0 +1,8 @@
+# from utils import app_utils
+# import matplotlib.pyplot as plt
+
+# image = app_utils.get_image('NAAMES_ml/D20151104T112022_IFCB107/IFCB107D20151104T112022P00433.png')
+# print(app_utils.get_dissimilarities())
+# print(app_utils.get_models())
+# print(app_utils.get_user('user154@example.com'))
+# print(app_utils.create_user({'email': 'userx@example.com', 'Name': 'Bob', 'experience':0, 'lab': 'Lab A'}))
