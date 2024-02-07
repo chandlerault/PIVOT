b7f7a59bfe66a60c1c181f3d4b9db928369e0d0f
DreamWeaver2k <chandlerault@gmail.com>
2024-02-06 20:03:50 -0800

    pylint

--- PIVOT/utils/data_utils.py
+++ PIVOT/utils/data_utils.py
@@ -1,42 +1,26 @@
 """
-Data Utilities Module for Blob Storage Data Retrieval
-
-This module provides functions for retrieving data from Azure Blob Storage, including metrics data in Parquet format
-and images in various formats. It uses the Azure SDK, PyArrow for Parquet handling, and PIL (Pillow) for image processing.
+This module provides functions for interacting with databases, Azure Blob Storage, and image preprocessing.
 
 Configuration:
-    The module reads configuration settings from 'config/config.yaml' for Azure Blob Storage access.
+    The module reads configuration settings from 'config/config.yaml' for Azure Blob Storage and Database access.
 
 Functions:
-    - save_df(df, blob_name, container_name=None, connection_string=None): Save a df to blob_name in container_name. container_name defaults to metrics_container
-    - get_df(blob_name, container_name, connection_string=None): Retrieves a dataframe from blob storage that has blob_name in container container_name
-    - get_metrics(): Retrieve metrics data from Azure Blob Storage and convert it to a Pandas DataFrame.
-    - get_images(filtered_df, batch_size=1): Retrieve images from Azure Blob Storage based on a filtered DataFrame returns an iterator.
+    - get_blob_bytes(blob_path)
+    - insert_data(table_name, data) 
+    - select(table_name, conditions, columns=['*'])
+    - select_distinct(table_name, columns)
+    - get_status()
+    - preprocess_input(image, fixed_size=128)
 
 Usage:
-    - Import this module and use its functions to retrieve data from Azure Blob Storage.
+    - Import this module and use its functions to retrieve data from Azure Blob Storage or query database.
     - Ensure that the 'config/config.yaml' file contains the necessary configuration settings.
-
-Example:
-    from data_utils import get_metrics, get_images
-
-    metrics_df = get_metrics()
-    filtered_df = metrics_df[metrics_df['confidence_interval']<.1]
-    image_iterator = get_images(filtered_df, 10)
-    for image_set in image_iterator:
-        for image in image_set:
-            # show(image)
 """
-import yaml
 import pymssql
 from azure.storage.blob import BlobServiceClient
-from PIL import Image
-import pandas as pd
 import numpy as np
 import cv2
-
-
-from utils import CONFIG
+from utils import CONFIG # pylint: disable=no-name-in-module
 
 def get_blob_bytes(blob_path):
     """
@@ -48,35 +32,42 @@ def get_blob_bytes(blob_path):
     Returns:
         PIL.Image.Image: A  PIL.Image.Image object  with image retrieved from a filepath.
     """
+    try:
 
-    if not isinstance(blob_path, str):
-        raise TypeError("filepath must be a string")
+        if not isinstance(blob_path, str):
+            raise TypeError("filepath must be a string")
 
-    connection_string = CONFIG['connection_string'] # TODO: eventually make this one connection string.
-    container_name = CONFIG['image_container']
+        connection_string = CONFIG['connection_string']
+        container_name = CONFIG['image_container']
 
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
-    blob_exists = blob_client.exists()
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
+        blob_exists = blob_client.exists()
 
-    if blob_exists:
-        pass
-    else:
-        print(f"Blob '{blob_path}' does not exist in the container.")
+        if blob_exists:
+            pass
+        else:
+            print(f"Blob '{blob_path}' does not exist in the container.")
+            return None
+
+        blob_data = blob_client.download_blob()
+        content = blob_data.readall()
+        blob_service_client.close()
+        return content
+
+    except TypeError as e:
+        print("Error:", str(e))
         return None
-
-    blob_data = blob_client.download_blob()
-    content = blob_data.readall()
-    blob_service_client.close()
-    return content        
 
 def insert_data(table_name, data):
     """
     Inserts data into the table with the corresponding table_name.
 
     Args:
-        table_name (str): The name of the table in the database that the data should be inserted into.
-        data (dict/list<dict>): A single dict or list of dicts. Each key represents a column in the table and each value is a value to be inserted.
+        table_name (str): The name of the table in the database 
+            that the data should be inserted into.
+        data (dict/list<dict>): A single dict or list of dicts. Each key represents a 
+            column in the table and each value is a value to be inserted. 
     
     Returns:
         id (int, list<int>): id's of inserted data.
@@ -87,9 +78,8 @@ def insert_data(table_name, data):
         database = CONFIG['database']
         user = CONFIG['db_user']
         password = CONFIG['db_password']
-        
 
-        with pymssql.connect(server, user, password, database) as conn:
+        with pymssql.connect(server, user, password, database) as conn: # pylint: disable=no-member
             with conn.cursor() as cursor:
                 if isinstance(data, list) and len(data) > 0:
                     # Generate the INSERT statement dynamically based on the dictionary keys
@@ -99,10 +89,7 @@ def insert_data(table_name, data):
                     # Execute the INSERT statement using executemany and the list of dictionaries
                     cursor.executemany(insert_query, data)
                     conn.commit()
-
-                    # TODO: dump id into temporary tables in batch call and return id's
-
-                elif isinstance(data, dict):
+                if isinstance(data, dict):
                     # Generate the INSERT statement dynamically based on the dictionary keys
                     columns = ', '.join(data.keys())  # Assuming all dictionaries have the same keys
                     placeholders = ', '.join('%(' + key + ')s' for key in data.keys())
@@ -114,16 +101,32 @@ def insert_data(table_name, data):
                     cursor.execute("SELECT SCOPE_IDENTITY()")
                     ids = cursor.fetchone()[0]
                     return ids
-    except Exception as e:
-        print("Error:", str(e))
+    except pymssql.InterfaceError as ie: # pylint: disable=no-member
+        print("InterfaceError:", str(ie))
+    except pymssql.DatabaseError as de: # pylint: disable=no-member
+        print("DatabaseError:", str(de))
+    return None
+def select(table_name, conditions, columns=['*']): # pylint: disable=dangerous-default-value
+    """
+    Executes a SELECT query on a specified table in the database.
 
-def select(table_name, conditions, columns=['*']):
+    Args:
+        table_name (str): The name of the table to select from.
+        conditions (dict): A dictionary representing the conditions to apply to the query.
+            Keys are column names and values are the corresponding values to match.
+        columns (list, optional): A list of column names to retrieve. Defaults to ['*'].
+
+    Returns:
+        list: A list of dictionaries, each representing a row of the result set.
+            Keys of each dictionary are column names and values are the corresponding values in the row.
+            An empty list is returned if there are no results or if an error occurs.
+    """
     try:
         server = CONFIG['server']
         database = CONFIG['database']
         user = CONFIG['db_user']
         password = CONFIG['db_password']
-        with pymssql.connect(server=server, database=database, user=user, password=password) as conn:
+        with pymssql.connect(server=server, database=database, user=user, password=password) as conn: # pylint: disable=no-member
             with conn.cursor() as cursor:
                 query = f"SELECT {', '.join(columns)} FROM {table_name}"
 
@@ -139,17 +142,31 @@ def select(table_name, conditions, columns=['*']):
                 column_names = [desc[0] for desc in cursor.description]
                 result = [dict(zip(column_names, row)) for row in rows]
                 return result
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return []
+    except pymssql.InterfaceError as ie: # pylint: disable=no-member
+        print("InterfaceError:", str(ie))
+    except pymssql.DatabaseError as de: # pylint: disable=no-member
+        print("DatabaseError:", str(de))
+    return []
 
 def select_distinct(table_name, columns):
+    """
+    Executes a SELECT DISTINCT query on a specified table in the database.
+
+    Args:
+        table_name (str): The name of the table to select from.
+        columns (list): A list of column names to retrieve distinct values from.
+
+    Returns:
+        list: A list of dictionaries, each representing a row of the result set.
+            Keys of each dictionary are column names and values are the corresponding values in the row.
+            An empty list is returned if there are no results or if an error occurs.
+    """
     try:
         server = CONFIG['server']
         database = CONFIG['database']
         user = CONFIG['db_user']
         password = CONFIG['db_password']
-        with pymssql.connect(server=server, database=database, user=user, password=password) as conn:
+        with pymssql.connect(server=server, database=database, user=user, password=password) as conn: # pylint: disable=no-member
             with conn.cursor() as cursor:
                 query = f"SELECT DISTINCT {', '.join(columns)} FROM {table_name}"
                 cursor.execute(query)
@@ -157,27 +174,37 @@ def select_distinct(table_name, columns):
                 column_names = [desc[0] for desc in cursor.description]
                 result = [dict(zip(column_names, row)) for row in rows]
                 return result
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return []
+
+    except pymssql.InterfaceError as ie: # pylint: disable=no-member
+        print("InterfaceError:", str(ie))
+    except pymssql.DatabaseError as de: # pylint: disable=no-member
+        print("DatabaseError:", str(de))
+    return []
 
 def get_status():
+    """
+    Checks if the specified database is online.
+
+    Returns:
+        bool: True if the database is online, False otherwise.
+    """
     try:
         server = CONFIG['server']
         database = CONFIG['database']
         user = CONFIG['db_user']
         password = CONFIG['db_password']
-        with pymssql.connect(server=server, database=database, user=user, password=password) as conn:
+        with pymssql.connect(server=server, database=database, user=user, password=password) as conn: # pylint: disable=no-member
             with conn.cursor() as cursor:
                 cursor.execute("SELECT state_desc FROM sys.databases WHERE name = %s", (database,))
                 row = cursor.fetchone()
                 if row and row[0] == 'ONLINE':
                     return True
-                else:
-                    return False
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return False
+                return False
+    except pymssql.InterfaceError as ie: # pylint: disable=no-member
+        print("InterfaceError:", str(ie))
+    except pymssql.DatabaseError as de: # pylint: disable=no-member
+        print("DatabaseError:", str(de))
+    return False
 
 def preprocess_input(image, fixed_size=128):
     """
@@ -190,9 +217,9 @@ def preprocess_input(image, fixed_size=128):
     Returns:
         numpy.ndarray: Preprocessed image with the specified fixed size.
     """
-    image_size = image.shape[:2] 
+    image_size = image.shape[:2]
     ratio = float(fixed_size)/max(image_size)
-    new_size = tuple([int(x*ratio) for x in image_size])
+    new_size = tuple([int(x*ratio) for x in image_size]) # pylint: disable=consider-using-generator
     img = cv2.resize(image, (new_size[1], new_size[0]))
     delta_w = fixed_size - new_size[1]
     delta_h = fixed_size - new_size[0]
@@ -200,9 +227,6 @@ def preprocess_input(image, fixed_size=128):
     left, right = delta_w//2, delta_w-(delta_w//2)
     color = [0, 0, 0]
     ri = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
-    #print(ri.shape)
-    # gray_image = cv2.cvtColor(ri, cv2.COLOR_BGR2GRAY)
     gimg = np.array(ri).reshape(fixed_size,fixed_size,1)
-    #gimg = np.array(ri).reshape(fixed_size,fixed_size,1)
     img_n = cv2.normalize(gimg, gimg, 0, 255, cv2.NORM_MINMAX)
     return img_n
