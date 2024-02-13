ebfb9b0b6b9aa3a560dd257e0cf1409ba263dbf9
ad-iti <aditi.shrivastava00@hotmail.com>
2024-02-12 19:00:45 -0800

    update score.py and pylint

--- PIVOT/model_serving/score.py
+++ PIVOT/model_serving/score.py
@@ -1,119 +1,63 @@
 """
-This isn't being used at all and I will probably delete. 
+Contains the logic about how to run the model and
+read the input data submitted by the deployment executor.
+Each model deployment has a scoring script (and any other required dependencies).
 
-Only needed in the case of batch deployment.
-Contains the logic about how to run the model and 
-read the input data submitted by the batch deployment executor.
-Each model deployment has a scoring script
-(and any other required dependencies).
-
-Used as the inference file in Dockerfile and as scoring file during deployment. 
+Intended input for run() to be './scoring_data.json'. 
 """
-from azureml.core.model import Model
-import mlflow
-import mlflow.keras
-from tensorflow.keras.models import model_from_json
-from tensorflow.keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model
-import tensorflow.keras as keras
-import tensorflow as tf
-from keras.callbacks import Callback
-import imageio
-import numpy as np
-import cv2
-import pandas as pd
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
-from tqdm.auto import trange
-from tqdm.auto import tqdm
+import os
+import logging
+import joblib
+import requests
 import yaml
-import concurrent.futures
-import json
-
-def preprocess_input(image, fixed_size=128):
-    '''
-    '''
-    image_size = image.shape[:2] 
-    ratio = float(fixed_size)/max(image_size)
-    new_size = tuple([int(x*ratio) for x in image_size])
-    img = cv2.resize(image, (new_size[1], new_size[0]))
-    delta_w = fixed_size - new_size[1]
-    delta_h = fixed_size - new_size[0]
-    top, bottom = delta_h//2, delta_h-(delta_h//2)
-    left, right = delta_w//2, delta_w-(delta_w//2)
-    color = [0, 0, 0]
-    ri = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
-    # gray_image = cv2.cvtColor(ri, cv2.COLOR_BGR2GRAY)
-    gray_image = ri
-    gimg = np.array(gray_image).reshape(fixed_size, fixed_size, 1)
-    img_n = cv2.normalize(gimg, gimg, 0, 255, cv2.NORM_MINMAX)
-    return img_n
-
-def preprocess_batch(order_index, batch_urls):
-    idxs = []
-    start_index = 0
-    data = []
-    for i, c_url in enumerate(tqdm(batch_urls, leave=False)):
-        data.append(preprocess_input(np.expand_dims(imageio.v2.imread(c_url), axis=-1)))
-        idxs.append(order_index*batch_size + i + start_index)
-
-    result = (idxs, data)
-    return result
 
 def init():
-    global loaded_model
-    model_path = Model.get_model_path('ifcb-image-class')
-    with open(model_path + '/model-cnn-v1-b3.json', 'r') as json_file:
-        loaded_model_json = json_file.read()
-    loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights(model_path + '/model-cnn-v1-b3.h5')
+    """
+    This function is called when the container is initialized/started,
+    typically after create/update of the deployment.
+    """
+    global MODEL
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # Path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    model_path = os.path.join(
+        os.getenv("AZUREML_MODEL_DIR"), "model/"
+    )
 
-def run(cloud_urls):   
-    # Specify the batch size and number of workers
-    batch_size = 320
-    num_workers = 24  
+    # deserialize the model file back into a sklearn model
+    MODEL = joblib.load(model_path)
+    logging.info("Init complete")
 
-    # Split URLs into batches
-    start_index = 0
-    url_batches = [cloud_urls[i:i + batch_size] for i in range(start_index, len(cloud_urls), batch_size)]
 
-    # Create an empty list to store probabilities
-    all_probabilities = []
+def run(json_payload):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
 
-    # Create a list to store the order of URLs
-    url_order = []
+    Parameters:
+        json_payload (json): JSON file containing three sample data to be used for testing.
+    """
+    # Read in config, change when import works
+    with open("config.yaml", encoding='utf-8') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
 
-    try:
-        # Use concurrent.futures for parallel processing
-        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-            future_to_order_index = {
-                executor.submit(preprocess_batch, i, batch_urls): i 
-                for i, batch_urls in enumerate(url_batches)
-            }
-            for future in tqdm(concurrent.futures.as_completed(future_to_order_index), total=len(url_batches)):
-                order_index = future_to_order_index[future]
-                try:
-                    # Get batch of images
-                    url_idxs, img_data = future.result()
-                    url_order.extend(url_idxs)
+    scoring_uri = config['scoring_uri'].format(endpoint_name='basemodel-endpoint')
+    api_key = config['api_key']
 
-                    # predict images
-                    batch_probabilities = loaded_model.predict(np.array(img_data), verbose=0)
-                    all_probabilities.extend(batch_probabilities)
-                except Exception as e:
-                    print(f"Error processing batch {order_index}: {e}")
+    # The azureml-model-deployment header will force the request to go to a specific deployment.
+    headers = {'Content-Type':'application/json',
+               'Authorization':('Bearer '+ api_key),
+               'azureml-model-deployment': 'pivot-basemodel'}
 
-    except Exception as e:
-        print(f"Error: {e}")
+    # Make the prediction request
+    response = requests.post(scoring_uri,
+                             data=json_payload,
+                             headers=headers,
+                             timeout=10)
 
-    except KeyboardInterrupt:
-        print("Process interrupted by user.")
+    # Check the response status code
+    if response.status_code == 200:
+        result = response.json()
+    else:
+        print("Prediction request failed with status code:", response.status_code)
+        print(response.text)
 
-    sorted_indices = np.argsort(url_order)
-    ordered_all_probs = np.vstack(all_probabilities)[sorted_indices]
-    all_probs = np.vstack(all_probabilities)
-    all_probs_df = pd.DataFrame(all_probs)
-    all_probs_df.index = url_order
-
-    return all_probs_df
-    
+    return result
--- PIVOT/utils/model_utils.py
+++ PIVOT/utils/model_utils.py
@@ -1,7 +1,18 @@
 """
-Make predictions via requests to REST endpoint of deployed (stream endpoint) model.
+This file contains utility functions for making model predictions via requests
+to the REST endpoint of deployed (stream endpoint) model on Azure ML.
+
+Classes:
+    - NumpyArrayEncoder:
+
+Functions:
+    - get_model_info: Retrieves the name of the deployed Azure ML model endpoint given a model ID.
+    - preprocess_input: Preprocesses an input image by resizing it to a fixed size and normalizing pixel values.
+    - predict: Calls the REST endpoint of the specified model to make predictions for all specified images.
+    - get_predictions: Retrieves and formats model predictions.
 """
 import json
+from json import JSONEncoder
 import yaml
 import requests
 
@@ -12,73 +23,98 @@ import imageio
 
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml import MLClient
-from azureml.core import Workspace, Model, Experiment
-
-from tensorflow.keras.models import Model as KerasModel
+from azureml.core import Workspace, Experiment
 
 def get_model_info(m_id):
     """
+    Retrieves the name of the deployed Azure ML model endpoint given a model ID.
+
+    Parameters:
+        m_id (int): The identifier of a specific model to predict with.
+
+    Returns:
+        endpoint_name (str): The name of the model endpoint to be called to make predictions.
     """
     # Remove this once I can actually access config through import
-    with open("../model_serving/config.yaml") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    with open("../model_serving/config.yaml", encoding='utf-8') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
     ml_client = MLClient(subscription_id=config['subscription_id'],
                          resource_group=config["resource_group"],
                          workspace_name=config['workspace_name'],
                          credential=DefaultAzureCredential())
 
-    ws = Workspace.from_config('../model_serving/config.json')
+    workspace = Workspace.from_config('../model_serving/config.json')
     experiment_name = config['experiment_name']
-    experiment = Experiment(workspace=ws, name=experiment_name)
+    experiment = Experiment(workspace=workspace, name=experiment_name)
     # run_name = 'basemodel'
     # for i in experiment.get_runs():
         # how get the endpoint name from the model ID? idk yet
-    endpoint_name = 'basemodel_endpoint'
+    endpoint_name = 'basemodel-endpoint'
     return endpoint_name
 
 def preprocess_input(image, fixed_size=128):
     """
+    Preprocesses an input image by resizing it to a fixed size and normalizing pixel values.
+
+    Parameters:
+        image (numpy.ndarray): Input image as a NumPy array.
+        fixed_size (int): Target size for the image after resizing. Default is 128.
+
+    Returns:
+        numpy.ndarray: Preprocessed image with the specified fixed size.
     """
-    image_size = image.shape[:2] 
+    image_size = image.shape[:2]
     ratio = float(fixed_size)/max(image_size)
-    new_size = tuple([int(x*ratio) for x in image_size])
+    new_size = tuple(int(x * ratio) for x in image_size)
     img = cv2.resize(image, (new_size[1], new_size[0]))
     delta_w = fixed_size - new_size[1]
     delta_h = fixed_size - new_size[0]
     top, bottom = delta_h//2, delta_h-(delta_h//2)
     left, right = delta_w//2, delta_w-(delta_w//2)
     color = [0, 0, 0]
-    ri = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    resized = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
     # gray_image = cv2.cvtColor(ri, cv2.COLOR_BGR2GRAY)
-    gray_image = ri
-    gimg = np.array(gray_image).reshape(fixed_size, fixed_size, 1)
+    gray_image = resized
+    gimg = np.array(gray_image).reshape((fixed_size, fixed_size, 1))
     img_n = cv2.normalize(gimg, gimg, 0, 255, cv2.NORM_MINMAX)
     return img_n
 
 class NumpyArrayEncoder(JSONEncoder):
     """
-    Add link!
+    Helper function to serialize a np.ndarray into a JSON String.
+    Sourced from: https://pynative.com/python-serialize-numpy-ndarray-into-json/
+
+    Modifies the JSONEncoder scope by overriding the default() method to accept np.ndarray.
     """
-    def default(self, obj):
-        if isinstance(obj, numpy.ndarray):
-            return obj.tolist()
-        return JSONEncoder.default(self, obj)
+    def default(self, o):
+        if isinstance(o, np.ndarray):
+            return o.tolist()
+        return JSONEncoder.default(self, o)
 
 def predict(df, m_id):
     """
-    Afhwefuhr ef
+    Calls the REST endpoint of the specified model to make predictions for all specified images.
+    Input to be formatted as per the output of sql_utils.get_images_to_predict().
 
-    Args:
-        df: A pd.DataFrame containing image metadata.
-            Columns: IMAGE_ID, BLOB_FILEPATH, cloud_urls
+    Parameters:
+        df (pd.DataFrame): DataFrame containing image metadata.
+            Columns: IMAGE_ID (int): The image ID.
+                     BLOB_FILEPATH (str): The filepath of the image.
+                     cloud_urls (str): The full URL of the image's blob storage endpoint.
+        m_id (int): The model ID.
+
+    Returns:
+        df (pd.DataFrame): DataFrame containing model predictions for each image.
+            Columns: i_id (int): The image ID.
+                     probs: A list of 10 class probabilities.
     """
-    # Remove this once I can actually access config through import 
-    with open("../model_serving/config.yaml") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    # Remove this once I can actually access config through import
+    with open("../model_serving/config.yaml", encoding='utf-8') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
 
     endpoint_name = get_model_info(m_id)
 
-    scoring_uri = config['scoring_uri'].format(endpoint_name='basemodel-endpoint')
+    scoring_uri = config['scoring_uri'].format(endpoint_name=endpoint_name)
     api_key = config['api_key']
 
     if not api_key:
@@ -98,9 +134,12 @@ def predict(df, m_id):
     # TODO: figure out how to get 'azureml-model-deployment'
     headers = {'Content-Type':'application/json',
                'Authorization':('Bearer '+ api_key),
-               'azureml-model-deployment': 'pivot-basemodel' }
+               'azureml-model-deployment': 'pivot-basemodel'}
     # Make the prediction request
-    response = requests.post(scoring_uri, data=json_payload, headers=headers)
+    response = requests.post(scoring_uri,
+                             data=json_payload,
+                             headers=headers,
+                             timeout=10)
 
     # Check the response status code
     if response.status_code == 200:
@@ -108,18 +147,27 @@ def predict(df, m_id):
     else:
         print("Prediction request failed with status code:", response.status_code)
         print(response.text)
-    df = pd.DataFrame({'i_id': df.IMAGE_ID.values, 
+    df = pd.DataFrame({'i_id': df.I_ID.values,
                          'probs': result})
     return df
 
 def get_predictions(df, m_id):
     """
-    Gets and formats predictions. 
-    df: columns [IMAGE_ID, BLOB_FILEPATH]
+    Retrieves and formats model predictions.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame containing image metadata.
+            Columns: IMAGE_ID (int): The image ID.
+                     BLOB_FILEPATH (str): The filepath of the image.
+        m_id (int): The model ID.
+
+    Returns:
+        out (list): A list of dictionaries to be inputted into the predictions table.
     """
-    with open("../model_serving/config.yaml") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    df['cloud_urls'] = df.BLOB_FILEPATH.apply(lambda x: config['cloud_url'].format(filepath=x))
+    with open("../model_serving/config.yaml", encoding='utf-8') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+    df['cloud_urls'] = df.filepath.apply(lambda x: config['cloud_url'].format(filepath=x))
     preds = predict(df, m_id)
 
     classes = ['Chloro',
@@ -137,6 +185,6 @@ def get_predictions(df, m_id):
     preds['predlabel'] = preds.probs.apply(lambda x: classes[pd.Series(x).idxmax()])
     preds['m_id'] = [m_id] * len(preds)
     preds = preds.drop(['probs'], axis=1)
-    ls = preds.to_dict(orient='records')
+    out = preds.to_dict(orient='records')
 
-    return ls
+    return out
